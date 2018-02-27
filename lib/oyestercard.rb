43040c18937a6793713629de85dd59ee9836c872
Alfie Darko <me@alfiedarko.co.uk>
2018-02-27 15:51:15 +0000

    added all methods needed up to step 13

--- lib/oyestercard.rb
+++ lib/oyestercard.rb
@@ -1,12 +1,13 @@
 class Oyestercard
-
-  attr_reader :balance, :journey_status
+  attr_accessor :balance, :journeys
   MAXIMUM_BALANCE = 90
   MINIMUM_FARE = 1
 
   def initialize
+    @exit_station = nil
+    @journeys = []
+    @entry_station = nil
     @balance = 0
-    @journey_status = :not_in_transit
   end
 
   def top_up(amount)
@@ -14,23 +15,25 @@ class Oyestercard
     @balance += amount
   end
 
-  def touch_in
-    raise "you dont have enough credit" if @balance < MINIMUM_FARE
-    @journey_status = :in_transit
+  def touch_in(station)
+    @entry_station = station
+    raise 'you dont have enough credit' if @balance < MINIMUM_FARE
   end
 
-  def touch_out
+  def touch_out(station)
+    @exit_station = station
+    @journeys.push(entry_station: @entry_station, exit_station: @exit_station)
+    @exit_station, @entry_station = nil
     deduct(MINIMUM_FARE)
-    @journey_status = :not_in_transit
   end
 
   def in_journey?
-    journey_status == :in_transit
+    @entry_station != nil
   end
 
   private
+
   def deduct(amount)
     @balance -= amount
   end
-
 end
--- spec/oyestercard_spec.rb
+++ spec/oyestercard_spec.rb
@@ -1,14 +1,16 @@
 require 'oyestercard'
 
 describe Oyestercard do
-
   maximum_balance = Oyestercard::MAXIMUM_BALANCE
   minimum_fare = Oyestercard::MINIMUM_FARE
+  let(:fake_entry_station) { double }
+  let(:fake_exit_station) { double }
+
   it 'has a balance of zero' do
     expect(subject.balance).to eq(0)
   end
 
-  describe "#top_up" do
+  describe '#top_up' do
     it 'responds to top_up method' do
       expect(subject).to respond_to(:top_up).with(1).argument
     end
@@ -19,60 +21,76 @@ describe Oyestercard do
   end
   it 'raise an error when try to top_up more then balance limit' do
     subject.top_up(maximum_balance)
-    expect{ subject.top_up 1 }.to raise_error " Maximum balance of #{maximum_balance} exceeded "
+    expect { subject.top_up 1 }.to raise_error " Maximum balance of #{maximum_balance} exceeded "
   end
-  # because we set deduct method to br private, we dont need to test this method anymore and we should remove it
-  # describe "#deduct" do
-  #   it 'responds to deduct method' do
-  #     expect(subject).to respond_to(:deduct).with(1).argument
-  #   end
-  #   it 'deduct amount from balance' do
-  #     subject.top_up(minimum_fare)
-  #     expect {subject.deduct(1) }.to change { subject.balance }.by(-1)
-  #   end
-  # end
 
-  describe "#touch_in" do
+  describe '#touch_in' do
     it 'responds to touch_in' do
       expect(subject).to respond_to(:touch_in)
     end
     it 'shows the status when you touch in' do
       subject.top_up(minimum_fare)
-      expect{ subject.touch_in }.to change { subject.journey_status }.to(:in_transit)
+      subject.touch_in(fake_entry_station)
+      expect(subject).to be_in_journey
     end
     it 'checking minimum balance on touch in' do
-      expect{ subject.touch_in }.to raise_error "you dont have enough credit"
+      expect { subject.touch_in(fake_entry_station) }.to raise_error 'you dont have enough credit'
+    end
+
+    it 'saves entry station to a variable' do
+      subject.top_up(50)
+      subject.touch_in(fake_entry_station)
+      expect(subject.entry_station).to eq(fake_entry_station)
     end
   end
 
-  describe "#touch_out" do
+  describe '#touch_out' do
     it 'responds to touch_out' do
       expect(subject).to respond_to(:touch_out)
     end
     it 'shows the status when you touch_out' do
       subject.top_up(minimum_fare)
-      subject.touch_in
-      expect{ subject.touch_out }.to change { subject.journey_status }.to(:not_in_transit)
+      subject.touch_in(fake_entry_station)
+      subject.touch_out(fake_exit_station)
+      expect(subject).not_to be_in_journey
     end
     it 'charging for the journey' do
-      expect{ subject.touch_out }.to change { subject.balance }.by(-minimum_fare)
+      expect { subject.touch_out(fake_exit_station) }.to change { subject.balance }.by(-minimum_fare)
+    end
+
+    it 'makes entry_station eq to nil' do
+      subject.top_up(50)
+      subject.touch_in(fake_entry_station)
+      subject.touch_out(fake_exit_station)
+      expect(subject.entry_station).to eq(nil)
     end
   end
 
-  describe "#in_journey?" do
+  describe '#in_journey?' do
     it 'responds to in_journey?' do
       expect(subject).to respond_to(:in_journey?)
     end
     it 'shows the status when you are in journey' do
       subject.top_up(minimum_fare)
-      subject.touch_in
+      subject.touch_in(fake_entry_station)
       expect(subject.in_journey?).to eq(true)
     end
     it 'shows the status when you are not in journey' do
-      subject.touch_out
+      subject.touch_out(fake_exit_station)
       expect(subject.in_journey?).to eq(false)
     end
   end
 
+  describe '#journeys' do
+    it "checks if journeys is empty at the start" do
+      expect(subject.journeys).to eq([])
+    end
 
+    it "records journeys inside a hash" do
+      subject.top_up(50)
+      subject.touch_in(fake_entry_station)
+      subject.touch_out(fake_exit_station)
+      expect(subject.journeys).to eq( [ { entry_station: fake_entry_station, exit_station: fake_exit_station } ] )
+    end
+  end
 end
