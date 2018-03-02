affe474251e9f5c6ce3fdd5af1ae5eb95706d83f
Alfie Darko <me@alfiedarko.co.uk>
2018-03-02 12:17:20 +0000

    added journeylog class and implemented all methods and rspec tests

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,5 +1,21 @@
-class JourneyLog
-  def initialize(:journey_class = Journey.new)
+require './lib/journey.rb'
 
+class JourneyLog
+  attr_accessor :journey_class
+
+  def initialize(journey_class = Journey.new)
+    @journey_class = journey_class
+  end
+
+  def start(station)
+    @journey_class.add_entry(station)
+  end
+
+  def finish(station)
+    @journey_class.add_exit(station)
+  end
+
+  def journeys
+    @journey_class.journeys
   end
 end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -1,4 +1,27 @@
 require './lib/journeylog.rb'
 
 describe JourneyLog do
+  let(:fake_entry_station) { double }
+  let(:fake_exit_station) { double }
+
+  describe '#start' do
+    it 'should start a journey with an entry station' do
+      subject.start(:fake_entry_station)
+      expect(subject.journey_class.journeys).to eq([{ entry_station: :fake_entry_station }])
+    end
+  end
+
+  describe '#finish' do
+    it 'should end a journey with an exit station' do
+      subject.start(:fake_entry_station)
+      subject.finish(:fake_exit_station)
+      expect(subject.journey_class.journeys).to eq([{ entry_station: :fake_entry_station, exit_station: :fake_exit_station }])
+    end
+  end
+
+  describe '#journeys' do
+    it 'should return the journeys' do
+      expect(subject.journeys).to eq []
+    end
+  end
 end
